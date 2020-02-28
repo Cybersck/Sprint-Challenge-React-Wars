02995c1f6cb447c396c89f5505ad916f33a1f258
kevin lautermilch <kevin.laut@outlook.com>
2020-02-28 14:27:39 -0500

    Updates

---
+++ precache-manifest.bd013b5f04a053ac16f88432871a03c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8eb9da2d79c8c5039c51a40f02b5f0",
+    "url": "/Sprint-Challenge-React-Wars/index.html"
+  },
+  {
+    "revision": "576eb6d8f7ecb8d31d4b",
+    "url": "/Sprint-Challenge-React-Wars/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8f7014d94be5276d8f2d",
+    "url": "/Sprint-Challenge-React-Wars/static/css/main.24637764.chunk.css"
+  },
+  {
+    "revision": "576eb6d8f7ecb8d31d4b",
+    "url": "/Sprint-Challenge-React-Wars/static/js/2.b3c7d4fe.chunk.js"
+  },
+  {
+    "revision": "8f7014d94be5276d8f2d",
+    "url": "/Sprint-Challenge-React-Wars/static/js/main.777c9c00.chunk.js"
+  },
+  {
+    "revision": "782454cc9036469b90c9",
+    "url": "/Sprint-Challenge-React-Wars/static/js/runtime-main.3ea5a01b.js"
+  },
+  {
+    "revision": "dff75a1ffda792b2380226033f28415c",
+    "url": "/Sprint-Challenge-React-Wars/static/media/sw-bg.dff75a1f.jpg"
+  }
+]);
